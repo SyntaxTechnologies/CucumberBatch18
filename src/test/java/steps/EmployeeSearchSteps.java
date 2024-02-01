54d7681c0ef27f02d0fcb7ced25b5b4c5fbbe77e
sohai <SyntaxTechnologies>
2024-01-31 21:38:40 -0500

    lc3101

--- src/test/java/steps/EmployeeSearchSteps.java
+++ src/test/java/steps/EmployeeSearchSteps.java
@@ -4,9 +4,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import utils.CommonMethods;
-
-import java.time.Duration;
 
 
 public class EmployeeSearchSteps extends CommonMethods {
@@ -15,34 +14,45 @@ public class EmployeeSearchSteps extends CommonMethods {
 
     @When("user clicks on PIM option")
     public void user_clicks_on_pim_option() {
-        driver.findElement(By.id("menu_pim_viewPimModule")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      //  driver.findElement(By.id("menu_pim_viewPimModule")).click();
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement pimOption =  driver.findElement(By.id("menu_pim_viewPimModule"));
+        click(pimOption);
     }
 
     @When("user clicks on employee list option")
     public void user_clicks_on_employee_list_option() {
-        driver.findElement(By.id("menu_pim_viewEmployeeList")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      //  driver.findElement(By.id("menu_pim_viewEmployeeList")).click();
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement empListOption = driver.findElement(By.id("menu_pim_viewEmployeeList"));
+        click(empListOption);
     }
 
     @When("user enters valid employee id")
     public void user_enters_valid_employee_id() {
-        driver.findElement(By.id("empsearch_id")).sendKeys("95757A");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement empIdLoc = driver.findElement(By.id("empsearch_id"));
+        sendText("95757A", empIdLoc);
+     //   driver.findElement(By.id("empsearch_id")).sendKeys("95757A");
+      //  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
+
     @When("user clicks on search button")
     public void user_clicks_on_search_button() {
-        driver.findElement(By.id("searchBtn")).click();
+        WebElement searchOption = driver.findElement(By.id("searchBtn"));
+        click(searchOption);
+        // driver.findElement(By.id("searchBtn")).click();
     }
 
     @Then("user should be able to see employee details")
     public void user_should_be_able_to_see_employee_details() {
         System.out.println("Employee can be seen clearly");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      //  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @When("user enters valid employee name")
     public void user_enters_valid_employee_name() {
-        driver.findElement(By.id("empsearch_employee_name_empName")).sendKeys("abel");
+    //    driver.findElement(By.id("empsearch_employee_name_empName")).sendKeys("abel");
+    WebElement empNameLoc = driver.findElement(By.id("empsearch_employee_name_empName"));
+    sendText("abel",empNameLoc);
     }
 }
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -5,11 +5,10 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utils.CommonMethods;
-
-import java.io.IOException;
-import java.time.Duration;
+import utils.ConfigReader;
 
 public class LoginSteps extends CommonMethods {
     //we don't need this driver, since it is coming from common methods class
@@ -25,14 +24,20 @@ public class LoginSteps extends CommonMethods {
 
     @When("user enters admin username and password")
     public void user_enters_admin_username_and_password() {
-       driver.findElement(By.id("txtUsername")).sendKeys("admin");
-       driver.findElement(By.id("txtPassword")).sendKeys("Hum@nhrm123");
-       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      // driver.findElement(By.id("txtUsername")).sendKeys("admin");
+        WebElement usernameField = driver.findElement(By.id("txtUsername"));
+        WebElement passwordField = driver.findElement(By.id("txtPassword"));
+        sendText(ConfigReader.read("userName"),usernameField);
+        sendText(ConfigReader.read("password"),passwordField);
+       //driver.findElement(By.id("txtPassword")).sendKeys("Hum@nhrm123");
+       //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @When("user clicks on login button")
     public void user_clicks_on_login_button() {
-        driver.findElement(By.id("btnLogin")).click();
+        WebElement loginButton =  driver.findElement(By.id("btnLogin"));
+        click(loginButton);
+        //driver.findElement(By.id("btnLogin")).click();
     }
 
     @Then("user is successfully logged in")
