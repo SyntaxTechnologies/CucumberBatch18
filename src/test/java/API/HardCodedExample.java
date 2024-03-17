6008e3b17b5f5b0d13beeace7afd92826fcf61a7
sohai <SyntaxTechnologies>
2024-03-17 13:17:00 -0400

    lc1703

--- src/test/java/API/HardCodedExample.java
+++ src/test/java/API/HardCodedExample.java
@@ -3,19 +3,42 @@ package API;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class HardCodedExample {
 
     String baseURI = RestAssured.baseURI
             = "http://hrm.syntaxtechs.net/syntaxapi/api";
+    static String employee_id;
     String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTA2ODgxOTYsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTcxMDczMTM5NiwidXNlcklkIjoiNjQ1MiJ9.PsOK1XpInWKd19jSgMvEKEm3OHckYLvoX0eRcstgRJk";
 
     @Test
-    public void createEmployee(){
+    public void bgetCreatedEmployee(){
+        //prepare the request
+        RequestSpecification request = given().
+                header("Content-Type","application/json").
+                header("Authorization",token).
+                queryParam("employee_id",employee_id);
+
+
+        //hitting the endpoint
+        Response response = request.when().get("/getOneEmployee.php");
+
+        //validate the response
+        response.then().assertThat().statusCode(200);
+        response.prettyPrint();
+
+    }
+
+    @Test
+    public void acreateEmployee(){
         //it will create the request
         RequestSpecification request = given().
                     header("Content-Type","application/json").
@@ -37,11 +60,14 @@ public class HardCodedExample {
         response.then().assertThat().statusCode(201);
         //this method is used to print the response in console
         response.prettyPrint();
+        //hamcrest matchers
+        response.then().assertThat().body("Message", equalTo("Employee Created"));
+        response.then().assertThat().body("Employee.emp_firstname",equalTo("mario"));
+        response.then().assertThat().body("Employee.emp_lastname",equalTo("germeen"));
+        response.then().assertThat().header("Connection",equalTo("Keep-Alive"));
+        //to fetch the employee id from response body, we need response variable
+        employee_id = response.jsonPath().getString("Employee.employee_id");
     }
-
-
-
-
 
 
 }
